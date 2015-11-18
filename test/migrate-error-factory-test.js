542f67b6fd3892281f587d193262f88f82301698
David Brockman Smoliansky <david@springworks.se>
2015-11-18 16:18:41 +0100

    Test all fixtures automatically
    Loop through the fixture dir and generate tests

--- test/migrate-error-factory-test.js
+++
@@ -1,7 +0,0 @@
-require('./fixture-tester')('migrate-error-factory', [
-  '01',
-  '02',
-  '03',
-  '04',
-  '05',
-]);
--- test/test-setup-arrow-test.js
+++
@@ -1,7 +0,0 @@
-require('./fixture-tester')('test-setup-arrow', [
-  '01',
-  '02',
-  '03',
-  '04',
-  '05',
-]);
