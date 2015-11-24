5146949f3b3c65ef4bf7acab8ddbcd9522d24149
David Brockman Smoliansky <david@springworks.se>
2015-11-24 19:28:40 +0100

    Fix bug in migrate-error-factory
    When `error-factory` was required as an object (`const name = require(...);`) then the `create` method was not properly replaced with the `createError` method.
    
    Now it should be safe to rerun the codemod.

--- test-fixtures/migrate-error-factory/migrate-error-factory-03.output.js
+++ test-fixtures/migrate-error-factory/migrate-error-factory-03.output.js
@@ -3,7 +3,7 @@ var ErrorFactory = require('@springworks/error-factory');
 exports.someFunction = function(callback) {
   doTheThing(function(err, val) {
     if (err) {
-      callback(ErrorFactory.create({
+      callback(ErrorFactory.createError({
         code: 500,
         message: 'Oh no!',
         cause: err
