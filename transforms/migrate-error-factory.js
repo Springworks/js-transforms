45897f2a3eb6a213679903ed212077f6b3a4d952
David Brockman Smoliansky <david@springworks.se>
2015-12-15 13:54:58 +0100

    Simple typecheck of args passed to create

--- transforms/migrate-error-factory.js
+++ transforms/migrate-error-factory.js
@@ -9,26 +9,27 @@ module.exports = function(file, api) {
   const j = api.jscodeshift;
   const root = j(file.source);
   const modified = helpers.createChangeTracker();
+  const file_path = file.path;
 
-  transformRequired(j, modified, root);
-  transformImported(j, modified, root);
+  transformRequired(j, file_path, modified, root);
+  transformImported(j, file_path, modified, root);
 
   return modified() ? root.toSource({ quote: 'single' }) : null;
 };
 
 
-function transformRequired(j, modified, root) {
+function transformRequired(j, file_path, modified, root) {
   const require_calls = root.find(j.CallExpression, {
     callee: { name: 'require' },
     arguments: [{ value: MODULE_NAME }],
   });
 
-  transformRequiredWithMember(j, modified, root, require_calls);
-  transformRequiredWithObject(j, modified, root, require_calls);
+  transformRequiredWithMember(j, file_path, modified, root, require_calls);
+  transformRequiredWithObject(j, file_path, modified, root, require_calls);
 }
 
 
-function transformRequiredWithMember(j, modified, root, require_calls) {
+function transformRequiredWithMember(j, file_path, modified, root, require_calls) {
   const filtered = require_calls.filter(path => {
     const ppv = path.parentPath.value;
     return ppv.type === 'MemberExpression' && ppv.property.name === 'create';
@@ -37,34 +38,32 @@ function transformRequiredWithMember(j, modified, root, require_calls) {
   filtered.forEach(path => {
     path.parentPath.value.property.name = 'createError';
     const local_name = path.parentPath.parentPath.parentPath.value[0].id.name;
-    transformCallExpressionsToCreateWithName(j, modified, root, local_name);
+    transformCallExpressionsToCreateWithName(j, file_path, modified, root, local_name);
   });
 }
 
 
-function transformCallExpressionsToCreateWithName(j, modified, root, local_name) {
+function transformCallExpressionsToCreateWithName(j, file_path, modified, root, local_name) {
   const usage = root.find(j.CallExpression, { callee: { name: local_name } });
-  usage.forEach(p => transformCallExpressionToCreate(j, p));
-  modified(usage.size() > 0);
+  usage.forEach(p => transformCallExpressionToCreate(j, file_path, modified, p));
 }
 
 
-function transformRequiredWithObject(j, modified, root, require_calls) {
+function transformRequiredWithObject(j, file_path, modified, root, require_calls) {
   const filtered = require_calls.filter(path => path.parentPath.value.type === 'VariableDeclarator');
 
   filtered.forEach(path => {
     const local_name = path.parentPath.value.id.name;
     const usage = root.find(j.MemberExpression, { object: { name: local_name }, property: { name: 'create' } });
     usage.forEach(p => {
+      transformCallExpressionToCreate(j, file_path, modified, p.parentPath);
       p.value.property.name = 'createError';
-      transformCallExpressionToCreate(j, p.parentPath);
     });
-    modified(usage.size() > 0);
   });
 }
 
 
-function transformImported(j, modified, root) {
+function transformImported(j, file_path, modified, root) {
   const import_declarations = root.find(j.ImportDeclaration, {
     specifiers: [
       {
@@ -85,19 +84,23 @@ function transformImported(j, modified, root) {
       delete specifier.local;
     }
 
-    transformCallExpressionsToCreateWithName(j, modified, root, local_name);
+    transformCallExpressionsToCreateWithName(j, file_path, modified, root, local_name);
   });
 }
 
 
-function transformCallExpressionToCreate(j, path) {
+function transformCallExpressionToCreate(j, file_path, modified, path) {
   const args = path.value.arguments;
-  const props = [];
 
+  assertWithCodeLocation(args.length <= 2, 'create() called with too many arguments. Use a string template instead of a format string.', j, file_path, path);
+
+  const props = [];
   if (args.length > 0) {
+    assertIdentifierOrLiteralWithCodeLocation(args[0], 'number', 'Expected first arg to be a number.', j, file_path, path);
     props.push(j.property('init', j.identifier('code'), args[0]));
   }
   if (args.length > 1) {
+    assertIdentifierOrLiteralWithCodeLocation(args[1], 'string', 'Expected second arg to be a string.', j, file_path, path);
     props.push(j.property('init', j.identifier('message'), args[1]));
   }
 
@@ -108,6 +111,23 @@ function transformCallExpressionToCreate(j, path) {
 
   const params = j.objectExpression(props);
   path.value.arguments = [params];
+
+  modified(true);
+}
+
+
+function assertIdentifierOrLiteralWithCodeLocation(node, expected_literal_type, message, j, file_path, path) {
+  assertWithCodeLocation(node.type === 'Identifier' || (node.type === 'Literal' && typeof node.value === expected_literal_type), message, j, file_path, path);
+}
+
+
+function assertWithCodeLocation(value, message, j, file_path, path) {
+  /* istanbul ignore if */
+  if (!value) {
+    const error = new Error(message + '\n\n' + j(path.parent).toSource().split('\n').map(s => '>   ' + s).join('\n'));
+    error.stack = file_path + ':' + path.value.loc.start.line + '\n\n';
+    throw error;
+  }
 }
 
 
